fc5a1449341eeefa08d7aa0413f699da40d5e861
unknown <mgt306@nyu.edu>
2025-03-12 17:43:01 -0400

    set up CRUD routes on server

--- server.js
+++ server.js
@@ -20,6 +20,60 @@ mongoose.connect(process.env.MONGODB_URI, { userNewUrlParser: true, useUnifiedTo
 //Middleware
 app.use(express.json());
 
+//schema and model
+const todoSchema = new mongoose.Schema({
+  text: String,
+  completed: Boolean,
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+//CRUD routes
+//create a new ToDo
+app.post('/api/todos', async (req, res) =>{
+  try{
+    const { text } = req.body;
+    const todo = new Todo({ text, completed: false });
+    await todo.save();
+    res.status(201).json(todo);
+  } catch (err) {
+    res.status(400).json({ error: 'Failed to create ToDo' });
+  }
+});
+
+//read all ToDos
+app.get('/api/todos', async (req, res) => {
+  try{
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to retrieve ToDos' });
+  }
+});
+
+//Update a ToDo
+app.put('/api/todos/:id', async (req, res) => {
+  try{
+    const { id } = req.params;
+    const { text, completed } = req.body;
+    const todo = await Todo.findByIdAndUpdate(id, { text, completed}, { new: true});
+    res.json(todo);
+  } catch (err) {
+    res.status(400).json({ error: 'Failed to update ToDo' });
+  }
+});
+
+//Delete a ToDo
+app.delete('/api/todos/:id', async (req, res) => {
+  try{
+    const { id } = req.params;
+    await Todo.findByIdAndDelete(id);
+    res.status(204).send();
+  } catch (err) {
+    res.status(400).json({ error: 'Failed to delete ToDo' });
+  }
+});
+
 app.listen(port, () =>{
   console.log(`Sever is running on port ${port}`)
 });
